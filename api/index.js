8ffad1a50dda86c20409ba1cc65421136f0b6531
kassimabdou <kassimabdou003@gmail.com>
2026-02-01 12:49:19 +0300

    Inchallah

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-import app from '../server.js';  // remonte d'un dossier car api est dans la racine
+import app from '../server.js';
+import { createServer } from 'http';
+import { parse } from 'url';
 
-export default app;
+const server = createServer(app);
+
+export default async function handler(req, res) {
+  const parsedUrl = parse(req.url, true);
+
+  // On fait passer la requête au serveur Express
+  await new Promise((resolve) => {
+    server.emit('request', req, res);
+    res.on('finish', resolve);
+  });
+}
